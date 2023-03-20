1ff995ab2d918da14a3caea918fc98a4c5a39302
IvanJMartinezS <ivanmartinezwork@gmail.com>
2023-03-19 21:31:18 -0400

    Firts Version Clon Feedly

---
+++ src/components/Header.vue
@@ -0,0 +1,70 @@
+<script setup>
+import { ref } from "vue";
+import { useStore } from "../store/store";
+
+const store = useStore();
+
+const url = ref("");
+
+function handleSubmit() {
+  console.log(url);
+  if (!!url.value) {
+    store.registerNewSource(url.value);
+  }
+}
+</script>
+
+<template>
+  <div class="header">
+    <img alt="icon feedly" src="../assets/feedly-48.png" >
+    <h1>Feedly</h1>
+    <form @submit.prevent="handleSubmit">
+      <input class="input" required type="url" v-model="url" />
+      <button type="submit">Add</button>
+    </form>
+  </div>
+</template>
+
+<style scoped>
+.header {
+  background: #14495E;
+  grid-area: header;
+  justify-self: stretch;
+
+  display: flex;
+  justify-content: center;
+
+  padding: 10px;
+}
+
+form {
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  gap: 5px;
+}
+.input {
+  border: none;
+  width: 400px;
+}
+button {
+  border: none;
+  background-color: #41B883;
+  color: white;
+  padding: 5px 10px;
+}
+
+h1{
+  color: #41B883;
+  font-weight: normal;
+  font-size: 15px;
+  font-family: Arial;
+  padding-left: 7px;
+}
+
+img{
+  padding-left: 6px;
+  height: 30px;
+}
+
+</style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { useStore } from "../store/store";
+
+const store = useStore();
+
+function handleClick(source) {
+  const item = store.sources.find((i) => i === source);
+
+  store.loadSource(item);
+}
+</script>
+
+<template>
+  <div class="sidebar">
+    <div v-for="(source, index) in store.sources" :key="index">
+      <a href="#" @click="() => handleClick(source)">{{ source.name }}</a>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.sidebar {
+  grid-column-start: 1;
+  grid-column-end: 2;
+  background-color: #34495E;
+}
+
+.sidebar a {
+  color: #41B883;
+  display: block;
+  padding: 5px;
+  text-decoration: none;
+}
+
+.sidebar a:hover {
+  background-color: rgb(0, 0, 0);
+}
+</style>
